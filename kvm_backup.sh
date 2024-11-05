1ab4c47133666efb585824cf959ef328cf3dc4b3
Alexander Potemkin <alex.p@getzen.dev>
2024-11-05 22:16:41 +0100

    improving kvm_backup.sh

--- kvm_backup.sh
+++ kvm_backup.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 set -x
-set -e
+#set -e
+
+DISK_TO_PGP='DISK NAME'
+KEY_TO_PGP='PGP KEY'
+RCLONE_DEST='RCLONE DESTINATION PATH'
 
 SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd $SCRIPTDIR
 
-BACKUPS_DIR="./kvm_backup"
+rclone delete enc_storage_box: --include '*.{bak,xml,gpg}'
 
 for vm in $(virsh list --all --name) ; do
   disk_name=$(virsh dumpxml "$vm" | xmlstarlet sel -t -m '/domain/devices/disk' -m 'source/@*' -v '.' -n | head -1)
   echo "$vm -> $disk_name"
-  VM_BACKUP_DIR="$BACKUPS_DIR/$vm"
-  mkdir -p $VM_BACKUP_DIR
 
-  virsh dumpxml $vm > "$VM_BACKUP_DIR/$vm.xml"
+  virsh dumpxml $vm > "$vm.xml"
+  rclone move "$vm.xml" $RCLONE_DEST 
   virsh backup-begin $vm
-  virsh event $vm --event block-job
+  time virsh event $vm --event block-job
   virsh domjobinfo $vm --completed
 
   backup_disk_name=$(ls -rt $disk_name.* | tail -1)
-  time bzip2 -9 $backup_disk_name
-  mv $backup_disk_name.bz2 $VM_BACKUP_DIR/
+
+  if [[ $backup_disk_name == *"$DISK_TO_PGP"* ]]; then
+  	# gpg --import private.gpg && gpg --output new_file_name --decrypt encrypted_file # to decrypt
+  	time gpg -z 9 --batch --yes --trust-model always --output "$backup_disk_name.gpg" --encrypt --recipient $KEY_TO_PGP "$backup_disk_name" 
+  	ls -lh `dirname "$disk_name"`
+	rm $backup_disk_name
+	time rclone move "$backup_disk_name.gpg" $RCLONE_DEST 
+  else
+  	mv $backup_disk_name "$backup_disk_name.bak" 	
+	time rclone move "$backup_disk_name.bak" $RCLONE_DEST 
+  fi
 
 done
